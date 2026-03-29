7dfb663838e41cef0e0babee4f355908873b1e80
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-29 20:45:42 +0500

    Update lib/utils/colors.dart via MYTH Build System (Branch: project-69c948ea689a6f32e5c9d3e5)

---
+++ lib/utils/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primary = const Color(0xFF6C63FF);
+  static Color secondary = const Color(0xFFFF6584);
+  static Color accent = const Color(0xFF00D9A5);
+  static Color background = const Color(0xFFF8F9FE);
+  static Color surface = const Color(0xFFFFFFFF);
+  static Color textPrimary = const Color(0xFF2D3142);
+  static Color textSecondary = const Color(0xFF9094A6);
+  static Color xColor = const Color(0xFFFF6B6B);
+  static Color oColor = const Color(0xFF4ECDC4);
+  static Color gridLine = const Color(0xFFE0E0E0);
+  static Color winHighlight = const Color(0xFF56CCF2);
+}
