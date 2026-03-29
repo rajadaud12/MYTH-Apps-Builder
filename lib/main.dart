b31e425c28994f9425a43298989fd3bcd8486a61
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-29 12:43:16 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69c8d7d55feae435faedfed3)

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:simpletodo_app/utils/colors.dart'; 
+import 'package:simpletodo_app/screens/todo_screen.dart'; 
+
+void main() {
+  runApp(const SimpleTodoApp());
+}
+
+class SimpleTodoApp extends StatelessWidget {
+  const SimpleTodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'Simple Todo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          primary: AppColors.primary,
+          secondary: AppColors.secondary,
+          surface: AppColors.surface,
+          background: AppColors.background,
+        ),
+        scaffoldBackgroundColor: AppColors.background,
+        useMaterial3: true,
+      ),
+      home: const TodoScreen(),
+    );
+  }
+}
