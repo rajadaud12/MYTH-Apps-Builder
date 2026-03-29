8430b483d76e94b1fcfabae9a997e5b571a0e64e
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-29 11:04:04 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69c8c07c3675cf5e390b9069)

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:swipeit_app/screens/home_screen.dart'; 
+import 'package:swipeit_app/utils/colors.dart'; 
+
+void main() {
+  runApp(const SwipeItApp());
+}
+
+class SwipeItApp extends StatelessWidget {
+  const SwipeItApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'SwipeIt',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+        scaffoldBackgroundColor: AppColors.background,
+        fontFamily: 'Roboto',
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
