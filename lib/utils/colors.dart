9be9af269a477a1ab0ebeabd4eb77eca8b529cbf
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-31 00:00:46 +0500

    Update lib/utils/colors.dart via MYTH Build System (Branch: project-69cac81901422a34c8fe9be3)

---
+++ lib/utils/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primary = Color(0xFF6C63FF);
+  static Color primaryLight = Color(0xFF9D97FF);
+  static Color primaryDark = Color(0xFF4A42D9);
+  static Color secondary = Color(0xFFFF6B6B);
+  static Color background = Color(0xFFF8F9FE);
+  static Color surface = Colors.white;
+  static Color textPrimary = Color(0xFF2D3436);
+  static Color textSecondary = Color(0xFF636E72);
+  static Color textHint = Color(0xFFB2BEC3);
+  static Color border = Color(0xFFE0E0E0);
+  static Color error = Color(0xFFFF5252);
+  static Color success = Color(0xFF00C853);
+  static Color gradientStart = Color(0xFF6C63FF);
+  static Color gradientEnd = Color(0xFF9D97FF);
+}
