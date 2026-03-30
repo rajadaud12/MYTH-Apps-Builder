d33171a683dc49a5382b2f05a2791b6da2bf8924
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-30 08:26:44 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69c9ed2ec095dc01e350d426)

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:simpleweather_app/utils/colors.dart'; 
+import 'package:simpleweather_app/screens/weather_screen.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'Simple Weather',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+      ),
+      home: const WeatherScreen(),
+    );
+  }
+}
