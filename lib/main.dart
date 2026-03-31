531b7825bd0f542ddba82e84da7c1396b0045327
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-31 16:25:04 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69cbaea7a32e4920c975a776)

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:docprompt_explorer_app/screens/home.dart'; 
+import 'package:docprompt_explorer_app/utils/colors.dart'; 
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'DocPrompt Explorer',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+        scaffoldBackgroundColor: AppColors.background,
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
