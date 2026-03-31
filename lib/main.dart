200afde508df7751dfee678573b87fe0899251d3
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-31 19:50:32 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69cbdeea0f77ac8263cfee1b)

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutterloginapp_app/utils/colors.dart'; 
+import 'package:flutterloginapp_app/screens/login_screen.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'Login App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          primary: AppColors.primary,
+          secondary: AppColors.secondary,
+        ),
+        scaffoldBackgroundColor: AppColors.background,
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
