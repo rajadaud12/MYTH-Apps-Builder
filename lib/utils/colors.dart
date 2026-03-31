24759c82f509d61999465e70eee1a2bfce8612f4
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-31 17:59:33 +0500

    Update lib/utils/colors.dart via MYTH Build System (Branch: project-69cbc4dddc8fb5ff542347a7)

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Colors.black;
+  static const Color textPrimary = Colors.white;
+  static const Color accent = Color(0xFFFFD700);
+  static const Color inputBackground = Color(0xFF1A1A1A);
+  static const Color buttonBackground = Color(0xFF2D2D2D);
+}
