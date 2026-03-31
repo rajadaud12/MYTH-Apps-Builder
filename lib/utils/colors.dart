7c2e4589febd13a24ded22202430b5f17633efa0
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-31 16:28:28 +0500

    Update lib/utils/colors.dart via MYTH Build System (Branch: project-69cbaea7a32e4920c975a776)

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -5,4 +5,6 @@ class AppColors {
   static const Color background = Color(0xFF0F0F0F);
   static const Color textPrimary = Colors.white;
   static const Color textSecondary = Colors.grey;
+  static const Color surface = Color(0xFF1A1A1A);
+  static const Color cardBackground = Color(0xFF1E1E1E);
 }
