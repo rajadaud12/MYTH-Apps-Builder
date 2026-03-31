dd8a85839cabeae9effcc6250c7e131ef039a2e1
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-31 16:28:27 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69cbaea7a32e4920c975a776)

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: AppColors.primary,
         scaffoldBackgroundColor: AppColors.background,
-        textTheme: GoogleFonts.poppinsTextTheme(),
+        textTheme: GoogleFonts.poppinsTextTheme(ThemeData.dark().textTheme),
       ),
       home: HomeScreen(),
     );
